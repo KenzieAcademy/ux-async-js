a8bd83e3a4969d953ed5bb8153a24525105d1c14
JessicaIreland <rollwithitbakery@hotmail.com>
2020-05-12 08:17:27 -0400

    async

--- code.js
+++ code.js
@@ -3,10 +3,15 @@ window.onload = function () { // Run this once the page has loaded.
     document.querySelector("#searchButton").addEventListener("click", searchGithub);
     function searchGithub() {
         const searchUserText = document.querySelector("#searchUser").value;
+        fetch("https://api.github.com/search/users?q=" + searchUserText)
+        .then((response) => response.json())
+        .then((result) => {
+            let githubUsers = result.items
+            renderUserList(githubUsers);
+        });
 
-        // Obtain a list of users from the Github API that match searchUserText
-        //  The final result will contain an array under the key 'items'
-        // Pass this array to `renderUserList`
+
+       
     }
 
     function renderUserList(githubUsers) {
@@ -15,6 +20,7 @@ window.onload = function () { // Run this once the page has loaded.
         for (let i = 0; i < githubUsers.length; i++) {
             let githubUser = githubUsers[i];
             html += "<li>";
+            html += `<img src=${githubUser.avatar_url}/>`;
             html += `<strong>${githubUser.login}</strong>`;
             html += `<a target="_blank" href="${githubUser.html_url}">`;
             html += "</li>";
@@ -24,3 +30,7 @@ window.onload = function () { // Run this once the page has loaded.
         document.querySelector("#resultsContainer").innerHTML = html;
     }
 }
+
+// Obtain a list of users from the Github API that match searchUserText
+        //  The final result will contain an array under the key 'items'
+        // Pass this array to `renderUserList`
