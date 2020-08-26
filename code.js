7572bc2b6c0a7b64922c150373362554e628a1fc
January <januaryobrien.ux@gmail.com>
2020-08-26 09:22:37 -0400

    finished

--- code.js
+++ code.js
@@ -4,6 +4,12 @@ window.onload = function () { // Run this once the page has loaded.
     function searchGithub() {
         const url = "https://api.github.com/search/users?q="
         const searchUserText = document.querySelector("#searchUser").value;
+        fetch(url + searchUserText)
+        .then((response) => response.json())
+        .then((result) => {
+            let users = result.items;
+            renderUserList(users);
+        });
 
         // Obtain a list of users from the Github API that match searchUserText
         //  The final result will contain an array under the key 'items'
@@ -16,12 +22,15 @@ window.onload = function () { // Run this once the page has loaded.
         for (let i = 0; i < githubUsers.length; i++) {
             let githubUser = githubUsers[i];
             html += "<li>";
-            html += `<strong>${githubUser.login}</strong>`;
-            html += `<a target="_blank" href="${githubUser.html_url}">`;
-            html += "</li>";
+            html += `<img src=${githubUser.avatar_url} class="user_avatar">`;
+            html += `<a target="_blank" href="${githubUser.html_url}">
+                <strong>
+            ${githubUser.login}</strong></a>`;
+            html += "</li>"
         }
         html += "</ul>";
 
         document.querySelector("#resultsContainer").innerHTML = html;
     }
+
 }
